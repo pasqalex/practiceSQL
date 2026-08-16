c4532d387872c8d403388a65f2dfa4c2276edbe2
pasqalex <akktorior@gmail.com>
2026-08-17 01:42:59 +0300

    Update 7_ALT_practiceSQL.sql

--- SQL_files/7_ALT_practiceSQL.sql
+++ SQL_files/7_ALT_practiceSQL.sql
@@ -1,12 +1,28 @@
-WITH top_expensive_products AS (
+WITH all_canceled_orders AS (
+    SELECT order_id
+    FROM user_actions
+    WHERE action = 'cancel_order'
+),
+all_not_canceled_orders AS (
+    SELECT order_id
+    FROM user_actions ua
+    WHERE NOT EXISTS (SELECT 1 FROM all_canceled_orders aco
+                            WHERE aco.order_id = ua.order_id)
+),
+top_expensive_products AS (
     SELECT product_id
     FROM products
     ORDER BY price DESC
-    LIMIT 5)
+    LIMIT 5
+)
 SELECT
-    o.order_id,
-    o.product_ids
+    order_id,
+    product_ids
 FROM orders o
-WHERE EXISTS (
-    SELECT 1 FROM top_expensive_products tep WHERE tep.product_id = ANY(o.product_ids))
-ORDER BY o.order_id
+WHERE EXISTS (SELECT 1 FROM top_expensive_products tep
+                    WHERE tep.product_id = ANY(o.product_ids)
+)
+AND EXISTS (SELECT 1 FROM all_not_canceled_orders anc
+                    WHERE anc.order_id = o.order_id
+)
+ORDER BY order_id
